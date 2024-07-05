d7f9451f1bd75a14c6058fa5b2f4df98a309aa66
Peter Kompasz <kompaszbence@gmail.com>
2024-07-05 23:05:17 +0300

    FoundationIngestor

--- src/ingestors/index.ts
+++ src/ingestors/index.ts
@@ -1,6 +1,7 @@
 import { MintIngestor } from '../lib/types/mint-ingestor';
 import { ProhibitionDailyIngestor } from './prohibition-daily';
 import { FxHashIngestor } from './fxhash';
+import { FoundationIngestor } from './foundation';
 
 export type MintIngestionMap = {
   [key: string]: MintIngestor;
@@ -9,6 +10,7 @@ export type MintIngestionMap = {
 export const ALL_MINT_INGESTORS: MintIngestionMap = {
   'prohibition-daily': new ProhibitionDailyIngestor(),
   fxhash: new FxHashIngestor(),
+  foundation: new FoundationIngestor(),
 };
 
 export * from './';
