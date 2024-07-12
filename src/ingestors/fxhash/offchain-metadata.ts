c0364797d942b8577c5325ca7f586fb96e17637a
Chris Maddern <chris@chrismaddern.com>
2024-07-11 21:18:43 -0400

    Improvements to FXHash mint ingestor

--- src/ingestors/fxhash/offchain-metadata.ts
+++ src/ingestors/fxhash/offchain-metadata.ts
@@ -5,20 +5,7 @@ export const getFxhashMintByContract = async (resources: MintIngestorResources, 
     const graphQLRes = await resources.fetcher({
       url: 'https://api.v2-temp.fxhash.xyz/graphql',
       method: 'POST',
-      headers: {
-        accept: '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/json',
-        priority: 'u=1, i',
-        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        Referer: 'https://www.fxhash.xyz/',
-        'Referrer-Policy': 'strict-origin-when-cross-origin',
-      },
+      headers: fxHashGraphQlheaders,
       data: {
         query: `
             query GenerativeToken($generativeTokenId: String) {
@@ -30,6 +17,21 @@ export const getFxhashMintByContract = async (resources: MintIngestorResources, 
                 name
                 mintOpensAt
                 openEditionsEndsAt
+                reserves {
+                  method
+                  amount
+                  data
+                }
+                pricingFixed {
+                  opensAt
+                  price
+                }
+                pricingDutchAuction {
+                  opensAt
+                  levels
+                  decrementDuration
+                  restingPrice
+                }
                 author {
                   account {
                     profile {
@@ -73,47 +75,115 @@ export const getFxHashMintsBySlug = async (resources: MintIngestorResources, slu
       return acc;
     }, '');
 
-    const graphQLRes = await resources.fetcher({
-      url: 'https://api.v2-temp.fxhash.xyz/graphql',
-      method: 'POST',
-      headers: {
-        accept: '*/*',
-        'accept-language': 'en-US,en;q=0.9',
-        'content-type': 'application/json',
-        priority: 'u=1, i',
-        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        Referer: 'https://www.fxhash.xyz/',
-        'Referrer-Policy': 'strict-origin-when-cross-origin',
-      },
-      data: {
-        query: `
-            query GenerativeToken($filters: SearchFilters!) {
-              search(filters: $filters) {
-                generativeTokens {
-                  slug
-                  id
-                  chain
-                  isFrame
-                  name
-                  metadata
-                  mintOpensAt
-                  openEditionsEndsAt
+    try {
+      const graphQLRes = await resources.fetcher({
+        url: 'https://api.v2-temp.fxhash.xyz/graphql',
+        method: 'POST',
+        headers: fxHashGraphQlheaders,
+        data: {
+          query: `
+          query GenerativeToken($filters: SearchFilters!) {
+            search(filters: $filters) {
+              generativeTokens {
+                slug
+                id
+                chain
+                name
+                metadata
+                mintOpensAt
+                reserves {
+                  method
+                  amount
+                  data
                 }
+                pricingFixed {
+                  opensAt
+                  price
+                }
+                pricingDutchAuction {
+                  opensAt
+                  levels
+                  decrementDuration
+                  restingPrice
+                }
+                openEditionsEndsAt
               }
             }
-          `,
-        variables: {
-          filters: {
-            searchQuery_eq: longestSlug,
+          }
+            `,
+          variables: {
+            filters: {
+              searchQuery_eq: longestSlug,
+            },
           },
         },
-      },
-    });
+      });
 
-    return graphQLRes.data.data.search.generativeTokens;
+      return graphQLRes.data.data.search.generativeTokens;
+    } catch (error) {
+      return [];
+    }
+}
+
+
+const fxHashGraphQlheaders = {
+  accept: '*/*',
+  'authority': 'api.fxhash.xyz',
+  'accept-language': 'en-US,en;q=0.9',
+  'content-type': 'application/json',
+  priority: 'u=1, i',
+  'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': '"macOS"',
+  'sec-fetch-dest': 'empty',
+  'sec-fetch-mode': 'cors',
+  'sec-fetch-site': 'same-site',
+  Referer: 'https://studio.apollographql.com/sandbox/explorer',
+  'Referrer-Policy': 'strict-origin-when-cross-origin',
+  'Origin': 'https://studio.apollographql.com',
+
+};
+
+enum FxHashPricingType {
+  FIXED = 'FIXED',
+  DUTCH_AUCTION = 'DUTCH_AUCTION',
+  FRAME = 'FRAME',
+}
+export function getPricingFromParams(
+  generativeToken: any,
+  whitelist: boolean
+) {
+  if (!generativeToken) {
+    throw new Error("generativeToken is null or undefined")
+  }
+  const { pricingFixed, pricingDutchAuction, reserves, isFrame } = generativeToken;
+  var price = 0;
+  let type: FxHashPricingType | undefined = isFrame ? FxHashPricingType.FRAME : undefined;
+  if (pricingFixed) {
+    type = type || FxHashPricingType.FIXED;
+    price = pricingFixed.price;
+  }
+  if (pricingDutchAuction) {
+    type = FxHashPricingType.DUTCH_AUCTION;
+    price = pricingDutchAuction.restingPrice;
+  }
+
+  return { priceWei: price, type: type }
+}
+
+const BASE_FRAME_CONTRACT_ADDRESS = '0x6e625892C739bFD960671Db5544E260757480725';
+const BASE_FIXED_PRICE_CONTRACT_ADDRESS = '0x4bDcaC532143d8d35ed759189EE22E3704580b9D';
+const BASE_DUTCH_AUCTION_CONTRACT_ADDRESS = '0x9667a1Cf26223c9de22207DD93cfEEc9237b8f4E';
+
+export function fxHashContractForPricingType(mintType: FxHashPricingType, chain: string) {
+  if (mintType === FxHashPricingType.FRAME) {
+    return BASE_FRAME_CONTRACT_ADDRESS;
+  }
+  if (mintType === FxHashPricingType.FIXED) {
+    return BASE_FIXED_PRICE_CONTRACT_ADDRESS;
+  }
+  if (mintType === FxHashPricingType.DUTCH_AUCTION) {
+    return BASE_DUTCH_AUCTION_CONTRACT_ADDRESS; 
+  }
+  throw new Error(`Unknown mint type: ${mintType}`);
 }
--- src/ingestors/fxhash/onchain-metadata.ts
+++ src/ingestors/fxhash/onchain-metadata.ts
@@ -5,19 +5,3 @@ const getContract = async (chainId: number, contractAddress: string, alchemy: Al
   const contract = new Contract(contractAddress, abi, ethersProvider);
   return contract;
 };
-
-export const getFxHashMintPriceInEth = async (
-  chainId: number,
-  contractAddress: string,
-  dropAddress: string,
-  reserveId: number,
-  alchemy: Alchemy,
-  abi: any,
-): Promise<any> => {
-
-  const contract = await getContract(chainId, contractAddress, alchemy, abi);
-  const tokenPrice = await contract.functions.prices(dropAddress, reserveId);
-
-  const totalPrice = parseInt(tokenPrice.toString());
-  return `${totalPrice}`;
-};
