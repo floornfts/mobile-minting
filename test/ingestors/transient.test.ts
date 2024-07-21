2d5bf39d230c25959a1176260f4680bce180ff8a
Johnpaul <polymenjohn1@gmail.com>
2024-07-21 21:00:44 +0100

    chore: added tests for ERC721TL and ERC7160TL contracts

--- test/ingestors/transient.test.ts
+++ test/ingestors/transient.test.ts
@@ -13,7 +13,7 @@ describe('Transient', function () {
     successUrls: ['https://www.transient.xyz/stacks/kansas-smile'],
     failureUrls: ['https://www.transient.xyz/stacks/kansas-smiles', 'https://www.transient.xyz/stacks'],
     successContracts: [
-      { chainId: 8453, contractAddress: '0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8' }
+      { chainId: 8453, contractAddress: '0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8' },
       // { chainId: 8453, contractAddress: '0x8a2e6797d5930527272642a41f36cedc84a3935e' },
     ],
     failureContracts: [
@@ -21,65 +21,136 @@ describe('Transient', function () {
       { chainId: 8453, contractAddress: 'derp' },
     ],
   });
-  it('createMintTemplateForUrl: Returns a mint template for a supported URL', async function () {
+  const testCases = [
+    {
+      name: 'ERC1155TL',
+      input: {
+        url: 'https://www.transient.xyz/stacks/kansas-smile',
+        resources,
+      },
+      chainId: 8453,
+      expected: {
+        name: 'Kansas Smile',
+        description:
+          "'Kansas Smile' explores how our longing for the past connects with modern technology. It shows how digital tools can recreate and reimagine the past, allowing us to experience nostalgia in new ways through AI.\n\nExhibited at 'Digital Dreams' Kansas City 2024, part of the Digital Collage exhibition.\n\n(Analog and digital processes with AI)",
+        contractAddress: '0x32953d7ae37b05075b88c34e800ae80c1cb1b794',
+        contractMethod: 'purchase',
+        contractParams: '["0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8", 1, address, 1, 0, []]',
+        priceWei: '20900000000000000',
+        featuredImageUrlPattern: /https:\/\/ipfs.io\/ipfs\/.+\/media/,
+        availableForPurchaseStart: 1717777800000,
+        availableForPurchaseEnd: 3294577800000,
+        outputContractAddress: '0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8',
+      },
+    },
+    {
+      name: 'ERC7160TL',
+      input: {
+        url: 'https://www.transient.xyz/stacks/16384',
+        resources,
+      },
+      chainId: 8453,
+      expected: {
+        name: '16384 Squares',
+        description:
+          'By exposing a 1px 8x8 grid to simulated physics turbulence, the grid interferes with the diodes in a ever changing moiré patterns, revealing a subtle breeze of random generated palm leaves behind the grid. \n\nThe colors of the leaves are then transferred back into the squares of the grid as large pixels.\n\n16384 are the amount of emitting diodes on the 128px LED artworks from Spøgelsesmaskinen. \n\nEach token holds both a 4K MP4 version and the original 128x128px GIF artwork for low res LED screens.\nOn mint a new generative variation of the art piece will be added by the artist to the token, also as both MP4 and GIF.\n\nEach piece is 60 seconds loop, of 2161 frames\n\nCollectors are able to require a physical custom assembled LED display made specifically for this artwork on https://spogel.xyz/led',
+        contractAddress: '0x384092784cfaa91efaa77870c04d958e20840242',
+        contractMethod: 'purchase',
+        contractParams: '["0xd2f9c0ef092d7ffd1a5de43b6ee546065461887d", 0, address, 1, 0, []]',
+        priceWei: '50900000000000000',
+        featuredImageUrlPattern: /^https:\/\/dv0xp0uwyoh8r\.cloudfront\.net\/stacks\/[0-9a-fA-F\-]+\/media/,
+        availableForPurchaseStart: 1720465200000,
+        availableForPurchaseEnd: 3297265200000,
+        outputContractAddress: '0xd2f9c0ef092d7ffd1a5de43b6ee546065461887d',
+      },
+    },
+    {
+      name: 'ERC721TL',
+      input: {
+        url: 'https://www.transient.xyz/stacks/volumina-8',
+        resources,
+      },
+      chainId: 8453,
+      expected: {
+        name: 'Volumina 8',
+        description: 'Volumina 8 is one of a series of manipulated image sequences derived from photography of Australian landscapes taken in Spring 2022. Various phone and laptop apps have been utilised to construct mosaics, kaleidoscopic patterns and other structures from the original photos, in an exploration of expanding and contracting symmetries implied and derived from organic forms and processes of the natural world.',
+        contractAddress: '0x384092784cfaa91efaa77870c04d958e20840242',
+        contractMethod: 'purchase',
+        contractParams: '["0x999f084f06ee49a3deef0c9d1511d2f040da4034", 0, address, 1, 0, []]',
+        priceWei: '150900000000000000',
+        featuredImageUrlPattern: /^https:\/\/dv0xp0uwyoh8r\.cloudfront\.net\/stacks\/[0-9a-fA-F\-]+\/media/,
+        availableForPurchaseStart: 1720600200000,
+        availableForPurchaseEnd: 3297400200000,
+        outputContractAddress: '0x999f084f06ee49a3deef0c9d1511d2f040da4034',
+      },
+    },
+  ];
+  describe(`createMintTemplateForUrl: Returns a mint template for`, async function () {
     const ingestor = new TransientIngestor();
-    const url = 'https://www.transient.xyz/stacks/kansas-smile';
+    testCases.forEach(({ name, input, expected }) => {
+      it(`${name} stack`, async function () {
+        const template = await ingestor.createMintTemplateForUrl(input.resources, input.url);
 
-    const template = await ingestor.createMintTemplateForUrl(resources, url);
+        // Verify that the mint template passed validation
+        const builder = new MintTemplateBuilder(template);
+        builder.validateMintTemplate();
 
-    // Verify that the mint template passed validation
-    const builder = new MintTemplateBuilder(template);
-    builder.validateMintTemplate();
+        expect(template.name).to.equal(expected.name);
+        expect(template.description).to.equal(expected.description);
 
-    expect(template.name).to.equal('Kansas Smile');
-    expect(template.description).to.equal(
-      "'Kansas Smile' explores how our longing for the past connects with modern technology. It shows how digital tools can recreate and reimagine the past, allowing us to experience nostalgia in new ways through AI.\n\nExhibited at 'Digital Dreams' Kansas City 2024, part of the Digital Collage exhibition.\n\n(Analog and digital processes with AI)",
-    );
-    const mintInstructions = template.mintInstructions as EVMMintInstructions;
+        const mintInstructions = template.mintInstructions as EVMMintInstructions;
 
-    expect(mintInstructions.contractAddress).to.equal('0x32953d7ae37b05075b88c34e800ae80c1cb1b794');
-    expect(mintInstructions.contractMethod).to.equal('purchase');
-    expect(mintInstructions.contractParams).to.equal('["0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8", 1, address, 1, 0, []]');
-    expect(mintInstructions.priceWei).to.equal('20900000000000000');
+        expect(mintInstructions.contractAddress).to.equal(expected.contractAddress);
+        expect(mintInstructions.contractMethod).to.equal(expected.contractMethod);
+        expect(mintInstructions.contractParams).to.equal(expected.contractParams);
+        expect(mintInstructions.priceWei).to.equal(expected.priceWei);
+        expect(template.mintOutputContract?.address).to.equal(expected.outputContractAddress);
 
-    expect(template.featuredImageUrl).to.be.matches(/https:\/\/ipfs.io\/ipfs\/.+\/media/);
+        expect(template.featuredImageUrl).to.match(expected.featuredImageUrlPattern);
 
-    expect(template.marketingUrl).to.equal(url);
-    expect(template.availableForPurchaseStart?.getTime()).to.equal(1717777800000);
-    expect(template.availableForPurchaseEnd?.getTime()).to.equal(3294577800000);
+        expect(template.marketingUrl).to.equal(input.url);
+        expect(template.availableForPurchaseStart?.getTime()).to.equal(expected.availableForPurchaseStart);
+        expect(template.availableForPurchaseEnd?.getTime()).to.equal(expected.availableForPurchaseEnd);
+      });
+    });
   });
 
-  it('createMintTemplateForContract: Returns a mint template for a supported contract', async function () {
+  describe('createMintTemplateForContract: Returns a mint template for a supported contract', async function () {
     const ingestor = new TransientIngestor();
-    const contract = {
-      chainId: 8453,
-      contractAddress: '0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8',
-      url: 'https://www.transient.xyz/stacks/kansas-smile',
-    };
+    for (const testCase of testCases) {
+      it(`${testCase.name} contract at ${testCase.expected.outputContractAddress}`, async function () {
+        const contract = {
+          chainId: testCase.chainId,
+          contractAddress: testCase.expected.outputContractAddress,
+          url: testCase.input.url,
+        }
+        const template = await ingestor.createMintForContract(resources, contract);
 
-    const template = await ingestor.createMintForContract(resources, contract);
+        // Verify that the mint template passed validation
+        const builder = new MintTemplateBuilder(template);
+        builder.validateMintTemplate();
 
-    // Verify that the mint template passed validation
-    const builder = new MintTemplateBuilder(template);
-    builder.validateMintTemplate();
+        expect(template.name).to.equal(testCase.expected.name);
+        expect(template.description).to.equal(
+          testCase.expected.description
+        );
+        const mintInstructions = template.mintInstructions as EVMMintInstructions;
 
-    expect(template.name).to.equal('Kansas Smile');
-    expect(template.description).to.equal(
-      "'Kansas Smile' explores how our longing for the past connects with modern technology. It shows how digital tools can recreate and reimagine the past, allowing us to experience nostalgia in new ways through AI.\n\nExhibited at 'Digital Dreams' Kansas City 2024, part of the Digital Collage exhibition.\n\n(Analog and digital processes with AI)",
-    );
-    const mintInstructions = template.mintInstructions as EVMMintInstructions;
+        expect(mintInstructions.contractAddress).to.equal(testCase.expected.contractAddress);
+        expect(mintInstructions.contractMethod).to.equal('purchase');
+        expect(mintInstructions.contractParams).to.equal(
+          testCase.expected.contractParams
+        );
+        expect(mintInstructions.priceWei).to.equal(testCase.expected.priceWei);
 
-    expect(mintInstructions.contractAddress).to.equal('0x32953d7ae37b05075b88c34e800ae80c1cb1b794');
-    expect(mintInstructions.contractMethod).to.equal('purchase');
-    expect(mintInstructions.contractParams).to.equal('["0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8", 1, address, 1, 0, []]');
-    expect(mintInstructions.priceWei).to.equal('20900000000000000');
+        expect(template.mintOutputContract?.address).to.equal(testCase.expected.outputContractAddress);
 
-    expect(template.mintOutputContract?.address).to.equal('0x7c3a99d4a7adddc04ad05d7ca87f4949c1a62fa8');
+        expect(template.featuredImageUrl?.length).to.be.greaterThan(0);
 
-    expect(template.featuredImageUrl?.length).to.be.greaterThan(0);
-
-    expect(template.marketingUrl).to.equal(contract.url);
-    expect(template.availableForPurchaseStart?.getTime()).to.equal(1717777800000);
-    expect(template.availableForPurchaseEnd?.getTime()).to.equal(3294577800000);
+        expect(template.marketingUrl).to.equal(contract.url);
+        expect(template.availableForPurchaseStart?.getTime()).to.equal(testCase.expected.availableForPurchaseStart);
+        expect(template.availableForPurchaseEnd?.getTime()).to.equal(testCase.expected.availableForPurchaseEnd);
+      });
+    }
   });
 });
