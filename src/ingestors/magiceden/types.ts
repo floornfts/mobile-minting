b7fda18e4bc35f14001571544e925b990d0214d8
Peter Kompasz <kompaszbence@gmail.com>
2024-07-30 12:12:35 +0300

    MagicEden ingestor

---
+++ src/ingestors/magiceden/types.ts
@@ -0,0 +1,21 @@
+export type Collection = {
+  id: string;
+  chainId: number;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  symbol: string;
+  image: string;
+  banner: string;
+  twitterUrl: string;
+  discordUrl: string;
+  externalUrl: string;
+  twitterUsername: string;
+  description: string;
+  sampleImages: string[];
+  primaryContract: string;
+  creator: string;
+  contractKind: string;
+};
+
