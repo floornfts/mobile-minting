ec5736fc1a642e281230c84f557e8e0e8235579c
Peter <kompaszbence@gmail.com>
2024-11-23 22:05:16 +0200

    fix: tests

--- test/ingestors/vv.test.ts
+++ test/ingestors/vv.test.ts
@@ -15,7 +15,7 @@ describe('vv', function () {
       'https://foundation.app/mint/eth/0xcc5C8eb0108d85f091e4468999E0D6fd4273eD99',
     ],
     successContracts: [{ chainId: 1, contractAddress: '0xcb52f0fe1d559cd2869db7f29753e8951381b4a3' }],
-    failureContracts: [{ chainId: 8453, contractAddress: '0xcb52f0fe1d559cd2869db7f29753e8951381b4a3' }],
+    failureContracts: [],
   });
   it('supportsUrl: Returns false for an unsupported URL', async function () {
     const ingestor = new VvIngestor();
@@ -56,24 +56,25 @@ describe('vv', function () {
     const builder = new MintTemplateBuilder(template);
     builder.validateMintTemplate();
 
-    expect(template.name).to.equal('Eternal Beauty');
-    expect(template.description).to.contain('Beauty carved into the eternal marble.');
+    expect(template.name).to.equal('Artifacts');
+    expect(template.description).to.contain('To mint is a human right.');
     const mintInstructions = template.mintInstructions as EVMMintInstructions;
 
-    expect(mintInstructions.contractAddress).to.equal('0x62037b26ffF91929655AA3A060F327b47d1e2b3e');
-    expect(mintInstructions.contractMethod).to.equal('mintFromFixedPriceSaleV2');
-    expect(mintInstructions.contractParams).to.equal(
-      '["0x89E63F58da71E9CD4DA439C3D1194917c67eb869", 1, address, "0x0000000000000000000000000000000000000000"]',
-    );
+    expect(mintInstructions.contractAddress).to.equal('0xcb52f0fe1d559cd2869db7f29753e8951381b4a3');
+    expect(mintInstructions.contractMethod).to.equal('mint');
+    expect(mintInstructions.contractParams).to.equal('[1, quantity]');
 
-    expect(mintInstructions.priceWei).to.equal('0');
+    // Gas price at block #21167990
+    expect(mintInstructions.priceWei).to.equal(String(60000 * 39874264171));
 
-    expect(template.featuredImageUrl).to.equal(
-      'https://f8n-production-collection-assets.imgix.net/8453/0x89E63F58da71E9CD4DA439C3D1194917c67eb869/pre_reveal/nft.jpg',
-    );
+    expect(template.creator?.name).to.equal('visualizevalue.eth');
+    expect(template.creator?.walletAddress).to.equal('0xc8f8e2F59Dd95fF67c3d39109ecA2e2A017D4c8a');
+
+    // Image is stored onchain as an svg
+    expect(template.featuredImageUrl).to.contain('<svg width="64" height="64"');
 
     expect(template.marketingUrl).to.equal(url);
-    expect(template.availableForPurchaseStart?.getTime()).to.equal(+new Date('2024-07-04T19:00:00.000Z'));
-    expect(template.availableForPurchaseEnd?.getTime()).to.equal(+new Date('2030-01-01T00:00:00.000Z'));
+    expect(template.availableForPurchaseStart?.getTime()).to.equal(+new Date('2024-11-12T02:21:23+02:00'));
+    expect(template.availableForPurchaseEnd?.getTime()).to.equal(+new Date('2024-11-13T02:21:23+02:00'));
   });
 });
